45d111c3260fb3daab9fe1cf09101b9a67292812
Eric <yanghui1221@gmail.com>
2014-02-17 22:42:59 +0800

    2.17

--- src/com/rugao/zhaoshang/ToolFragment.java
+++ src/com/rugao/zhaoshang/ToolFragment.java
@@ -1,9 +1,11 @@
 package com.rugao.zhaoshang;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 
 public class ToolFragment extends BaseFragment {
@@ -14,6 +16,15 @@ public class ToolFragment extends BaseFragment {
 		super.onCreateView(inflater, container, savedInstanceState);
 		View toolLayout = inflater.inflate(R.layout.tool_layout,
 				container, false);
+		toolLayout.findViewById(R.id.tr).setOnClickListener(
+				new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Intent i = new Intent(getActivity(),LoginActivity.class);
+						getActivity().startActivity(i);
+						getActivity().finish();
+					}
+				});
 		return toolLayout;
 	}
 
